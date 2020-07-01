06a1a3205f6c124104b9e8578454b91ebd284c71
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2020-06-30 23:38:16 -0400

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -1,21 +1,21 @@
 T = int(input())
 
 for t in range (T):
-    countdowns = 0
+    answer = 0
     kcd = False
     var = [int(s) for s in input().split() if s.isdigit()]
     array = [int(s) for s in input().split() if s.isdigit()]
     N = var[0]  # Numbers in the array
-    K = var[1]  # K-countdowns
-    for n in range (N):
+    K = var[1]  # K-countdown
+    for n in range (N - K + 1):
         if (array[n] == K):
             kcd = True
             for k in range (K):
                 if (array[n + k] != (K - k)):
                     kcd = False
                 elif (array[n + k] == 1):
-                    countdowns += 1
+                    answer += 1
                     kcd = False
 
 
-    print ("Case #" + str(t + 1) + ": " + str(countdowns))
+    print ("Case #" + str(t + 1) + ": " + str(answer))
